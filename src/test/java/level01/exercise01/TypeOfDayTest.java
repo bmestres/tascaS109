781cf2818ae92cd0550e6f4892475762e77843d7
Bernat Mestres <ber.mestres@gmail.com>
2026-07-11 18:03:18 +0200

    test: added test to check determineWeekendOrWeekDay() correctness

--- src/test/java/level01/exercise01/TypeOfDayTest.java
+++ src/test/java/level01/exercise01/TypeOfDayTest.java
@@ -1,5 +1,22 @@
 package level01.exercise01;
 
-public class TypeOfDayTest {
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+
+public class TypeOfDayTest {
+    @Test
+    void typeOfDayMustMatch(){
+
+        String testDay1 = Calendar.determineWeekendOrWeekDay(Day.MONDAY);
+        assertThat(testDay1).isEqualTo("Weekday");
+        String testDay2 = Calendar.determineWeekendOrWeekDay(Day.WEDNESDAY);
+        assertThat(testDay2).isEqualTo("Weekday");
+        String testDay3 = Calendar.determineWeekendOrWeekDay(Day.FRIDAY);
+        assertThat(testDay3).isEqualTo("Weekday");
+        String testDay4 = Calendar.determineWeekendOrWeekDay(Day.SATURDAY);
+        assertThat(testDay4).isEqualTo("Weekend");
+        String testDay5 = Calendar.determineWeekendOrWeekDay(Day.SUNDAY);
+        assertThat(testDay5).isEqualTo("Weekend");
+    }
 }
