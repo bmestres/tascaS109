cb6a30e0f2f0202c03cc3b821182674ab4e93984
Bernat Mestres <ber.mestres@gmail.com>
2026-07-11 18:32:23 +0200

    feat: implemented functionality to print enum type

---
+++ src/main/java/level01/exercise02/Level.java
@@ -0,0 +1,7 @@
+package level01.exercise02;
+
+public enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/level01/exercise02/Task.java
@@ -0,0 +1,14 @@
+package level01.exercise02;
+
+public class Task {
+
+    Level level;
+
+    public Task(Level level){
+        this.level = level;
+    }
+
+    public void showLevel(){
+        System.out.format("Task level is %s", this.level);
+    }
+}
