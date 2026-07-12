c3d67b1fbc374b7dc2863ded7a93b05bbe456647
Bernat Mestres <ber.mestres@gmail.com>
2026-07-12 16:06:17 +0200

    feat: instantiated a record of type Person

---
+++ src/main/java/level03/Main.java
@@ -0,0 +1,8 @@
+package level03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person01 = new Person("Andrew", 20);
+    }
+}
---
+++ src/main/java/level03/Person.java
@@ -0,0 +1,5 @@
+package level03;
+
+public record Person(String name, int age) {
+}
+
