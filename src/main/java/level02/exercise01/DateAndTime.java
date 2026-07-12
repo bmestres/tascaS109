7f31293fbbb75ccf32a6d8d92f0b4224760fb18d
Bernat Mestres <ber.mestres@gmail.com>
2026-07-12 09:59:51 +0200

    feat: display current date on console

---
+++ src/main/java/level02/exercise01/DateAndTime.java
@@ -0,0 +1,10 @@
+package level02.exercise01;
+
+import java.time.LocalDate;
+
+public class DateAndTime {
+
+    public static LocalDate getCurrentDate(){
+        return LocalDate.now();
+    }
+}
---
+++ src/main/java/level02/exercise01/Main.java
@@ -0,0 +1,8 @@
+package level02.exercise01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print(DateAndTime.getCurrentDate());
+    }
+}
