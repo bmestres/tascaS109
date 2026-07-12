5b3e6d38fae80e2cbb6cedbb7d825243fb58d45e
Bernat Mestres <ber.mestres@gmail.com>
2026-07-12 16:24:19 +0200

    feat: added age safeguard logic in constructor

--- src/main/java/level03/Person.java
+++ src/main/java/level03/Person.java
@@ -4,6 +4,12 @@ public record Person(String name, int age) {
 
     private static final int ADULT_AGE = 18;
 
+    public Person {
+        if (age < ADULT_AGE) {
+            throw new IllegalArgumentException("Age cannot be less than 18");
+        }
+    }
+
     public String getName(){
         return this.name;
     }
